90753751d009362c41d141def45e391bfd4d477b
mudlord <mudlord@rebote.net>
2019-02-14 22:50:02 +1100

    fart about with the resource a bit to how i originally wanted it in the bloody beginning.

--- foo_dsp_effect/resource.h
+++ foo_dsp_effect/resource.h
@@ -2,6 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by foo_dsp_effect.rc
 //
+#define IDOK2                           3
 #define IDCANCEL3                       6
 #define IDD_DSP                         101
 #define IDD_ECHO                        102
@@ -192,7 +193,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        125
+#define _APS_NEXT_RESOURCE_VALUE        126
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1200
 #define _APS_NEXT_SYMED_VALUE           101
