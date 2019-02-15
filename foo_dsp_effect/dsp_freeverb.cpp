0f6818c9ee2f11e88cad01696e6bc34077c4f977
mudlord <mudlord@rebote.net>
2019-02-15 22:29:44 +1100

    Cleaned up resize.

--- foo_dsp_effect/dsp_freeverb.cpp
+++ foo_dsp_effect/dsp_freeverb.cpp
@@ -195,10 +195,10 @@ namespace {
 			}
 
 
-			ret.m_min_width = MulDiv(420, DPI.cx, 96);
-			ret.m_min_height = MulDiv(340, DPI.cy, 96);
-			ret.m_max_width = MulDiv(420, DPI.cx, 96);
-			ret.m_max_height = MulDiv(340, DPI.cy, 96);
+			ret.m_min_width = MulDiv(350, DPI.cx, 96);
+			ret.m_min_height = MulDiv(240, DPI.cy, 96);
+			ret.m_max_width = MulDiv(350, DPI.cx, 96);
+			ret.m_max_height = MulDiv(240, DPI.cy, 96);
 
 			// Deal with WS_EX_STATICEDGE and alike that we might have picked from host
 			ret.adjustForWindow(*this);
--- foo_dsp_effect/dsp_tremelo.cpp
+++ foo_dsp_effect/dsp_tremelo.cpp
@@ -337,9 +337,9 @@ namespace {
 
 
 			ret.m_min_width = MulDiv(420, DPI.cx, 96);
-			ret.m_min_height = MulDiv(140, DPI.cy, 96);
+			ret.m_min_height = MulDiv(160, DPI.cy, 96);
 			ret.m_max_width = MulDiv(420, DPI.cx, 96);
-			ret.m_max_height = MulDiv(140, DPI.cy, 96);
+			ret.m_max_height = MulDiv(160, DPI.cy, 96);
 
 			// Deal with WS_EX_STATICEDGE and alike that we might have picked from host
 			ret.adjustForWindow(*this);
